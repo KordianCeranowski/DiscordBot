1abbf319d129e9ab72dab6d7aadc1b3e075f8bd6
BuildTools <unconfigured@null.spigotmc.org>
2021-12-02 19:15:51 +0000

    Add option to download subtitles

--- bot.py
+++ bot.py
@@ -1,17 +1,24 @@
 import painter
 import discord
 import requests
-import w2g
 from time import localtime
+import opensubtitles
 
 IMG_NAME = 'resources/temp.png'
 client = discord.Client()
 
 
-def download(url):
-    r = requests.get(url)
-    with open(IMG_NAME, 'wb') as outfile:
-        outfile.write(r.content)
+async def send_help(message, key):
+    await message.channel.send(
+        "```yml\n" + 
+        "\n".join([
+            "!help: send_help",
+            "!e: turn_image_to_emojis",
+            "!subs: get_subtitles",
+            "!nuke: cleanup_messages"
+        ])
+        + "```"
+    )
 
 
 async def cleanup_messages(message, key):
@@ -21,16 +28,12 @@ async def cleanup_messages(message, key):
             await mess.delete()
 
 
-async def send_help(message, key):
-    await message.channel.send("""
-        '!nuke': cleanup_messages,
-        '!help': send_help,
-        '!e': turn_image_to_emojis,
-        'w2g': generate_w2g_room
-        """)
-
-
 async def turn_image_to_emojis(message, key):
+    def download(url):
+        r = requests.get(url)
+        with open(IMG_NAME, 'wb') as outfile:
+            outfile.write(r.content)
+
     if not message.attachments:
         await message.channel.send('No attachment found!')
         return
@@ -54,13 +57,14 @@ async def turn_image_to_emojis(message, key):
         await message.channel.send('Error occured while processing image')
 
 
-async def generate_w2g_room(message, key):
+async def get_subtitles(message, key):
+    video_name = message.content[len(key)+1:]
     try:
-        link = message.content[len(key)+1:]
-        print(link)
-        await w2g.run(link, message.channel)
+        lyrics_link = opensubtitles.get_subtitles(video_name)
+        await message.channel.send(lyrics_link)
     except:
-        await message.channel.send('Error occured, try again')
+        await message.channel.send('Error occured')
+
 
 
 @client.event
@@ -74,11 +78,11 @@ async def on_message(message):
         '!nuke': cleanup_messages,
         '!help': send_help,
         '!e': turn_image_to_emojis,
-        'w2g': generate_w2g_room
+        '!subs': get_subtitles
     }
 
     for command in options:
-        if message.content.startswith(command):
+        if message.content.startswith(command) and message.author != client.user:
             t = localtime()
             just = lambda x: str(x).rjust(2, '0')
             print(f"[{just(t.tm_hour)}:{just(t.tm_min)}:{just(t.tm_sec)}]: {command}")
@@ -86,5 +90,5 @@ async def on_message(message):
 
 
 if __name__ == "__main__":
-    discord_id = open("discord_id.txt", "r").read()
+    discord_id = open("token.txt", "r").read()
     client.run(discord_id)
---
+++ opensubtitles.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.chrome.options import Options
+
+
+def get_subtitles(query: str):
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    with webdriver.Chrome(options=options) as driver:
+        driver.get("https://www.opensubtitles.org")
+
+        search_bar = driver.find_element_by_xpath("//*[@id=\"search_text\"]")
+        search_bar.send_keys(query)
+        language = driver.find_element_by_xpath("/html/body/div[1]/div[7]/form/div[1]/button")
+        language.click()
+        english = [element for element in driver.find_elements_by_tag_name("span") if element.text == "English"][0]
+        english.click()
+        search_bar.send_keys(Keys.ENTER)
+
+        download = [element for element in driver.find_elements_by_tag_name("a") if element.text == "Download"][0]
+        link = download.get_attribute("href")
+        return link
