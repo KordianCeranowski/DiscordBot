11103951fa41abef13b1ad69caa32f736bd69793
Marcin Borysiak <35338647+mborysiak-sp@users.noreply.github.com>
2023-03-17 13:42:28 +0100

    Fix file encoding

--- opensubtitles.py
+++ opensubtitles.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.chrome.options import Options
 
 
